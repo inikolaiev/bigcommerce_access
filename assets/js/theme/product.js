9cdcb33a6700f5be7b466188408682c023213ffc
Igor Nikolayev <inikolaiev@access.dev>
2023-02-17 17:04:37 +0200

    GT-534: product fix gift messages

--- assets/js/theme/product.js
+++ assets/js/theme/product.js
@@ -59,6 +59,8 @@ export default class Product extends PageManager {
         });
 
         this.productReviewHandler();
+
+        this.giftProduct();
     }
 
     ariaDescribeReviewInputs($form) {
@@ -81,5 +83,58 @@ export default class Product extends PageManager {
         if (this.url.indexOf('#bulk_pricing') !== -1) {
             this.$bulkPricingLink.trigger('click');
         }
+    }
+
+    giftProduct() {
+        const $modifierInputs = $('[data-product-option-change]');
+        const $isGiftInput = $modifierInputs.find('.modifier-is input');
+        const $giftDelivery = $modifierInputs.find('.modifier-Gift.Delivery');
+        const $giftMessage = $modifierInputs.find('.modifier-Gift.message');
+        const $giftEmail = $modifierInputs.find('.modifier-Email');
+        const $giftAddress = $modifierInputs.find('.modifier-Address');
+        $giftDelivery.find('select').find('option').get(0).remove();
+
+        if ($isGiftInput.val() === '121') {
+            $giftDelivery.hide();
+            $giftEmail.hide();
+            $giftMessage.hide();
+            $giftAddress.hide();
+        }
+
+        $isGiftInput.on('change', function (e) {
+            if (this.value === '121') {
+                $giftDelivery.show();
+                $giftMessage.show();
+                $giftEmail.show();
+            } else {
+                $giftDelivery.hide();
+                $giftDelivery.find('select').val('');
+                $giftMessage.hide();
+                $giftMessage.find('textarea').val('');
+                $giftEmail.hide();
+                $giftEmail.find('input').val('');
+                $giftAddress.hide();
+                $giftAddress.find('input').val('');
+                $giftAddress.find('input').prop('required', false);
+                $giftEmail.find('input').prop('required', false);
+            }
+        });
+
+        $giftDelivery.find('select').on('change', function (e) {
+            if (this.value === '125') {
+                $giftEmail.show();
+                $giftEmail.find('input').prop('required', true);
+                $giftEmail.find('input').prop('pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+                $giftAddress.find('input').prop('required', false);
+                $giftAddress.hide();
+                $giftAddress.find('input').val('');
+            } else if (this.value === '126') {
+                $giftAddress.show();
+                $giftEmail.hide();
+                $giftEmail.find('input').val('');
+                $giftEmail.find('input').prop('required', false);
+                $giftAddress.find('input').prop('required', true);
+            }
+        });
     }
 }
